cb89312ced1d304baa0104af70ada1c6fea9b483
Parviz Subkhankulov <ps310@duke.edu>
2022-10-09 12:51:56 +0500

    - transferring the project to pycharm. - adding dynamic cards for the most important data (reusable)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash
 import pandas as pd
 import plotly.express as px
 from dash import dcc, html
-
+from dash_iconify import DashIconify
 
 external_script = ["https://tailwindcss.com/", {"src": "https://cdn.tailwindcss.com"}]
 
@@ -24,91 +24,119 @@ df = pd.DataFrame(
     }
 )
 
-
 sensor_count = df.Sensor.count()
 avr_speed = int(df.Speed.sum() / sensor_count)
 active_sensors = 4
 inactive_sensors = 2
 
-fig = px.bar(df, x="Sensor", y="Speed",)
+fig = px.bar(df, x="Sensor", y="Speed", )
+
+
+def generate_infocard(text, data, change):
+    return html.Div(
+        children=[
+            html.Div([
+                html.Div(html.Span(text, className="text-[14px] font-bold text-[#A2AAAD]")),
+                html.Div(html.Span(f"{data}", className="counter text-[28px] font-light")),
+                html.Div(html.Span(f"Learn More", className="link text-[12px] border-b-2 border-gray")),
+            ], className="left flex flex-col justify-between"),
+            html.Div([
+                html.Div([
+                    DashIconify(
+                        icon="bi:arrow-up-right-square-fill",
+                        width=20,
+                    ),
+                    html.Span(f"{change}", className="percentage positive text-[14px] pl-[5px]")
+                ], className="flex"),
+                html.Div(DashIconify(
+                    icon="bi:bar-chart-fill",
+                    width=30,
+                ), className="self-end"),
+            ], className="right left flex flex-col justify-between"),
+        ],
+        className=f"flex flex-1 p-[10px] h-[120px] justify-between shadow-lg rounded-lg",
+    )
+
 
 app.layout = html.Div(
-    html.Div(
-        children=[
-            html.Div(
-                children=[
-                    html.H1(children="DKU Gym Sensors Dashboard", className=" py-3 text-5xl font-bold text-gray-800"),
-                    html.Div(
-                        children="""
-                                """,
-                        className="text-left prose prose-lg text-2xl  py-3 text-gray-600",
-                    ),
-                ],
-                className="w-full mx-14 px-16 shadow-lg bg-white -mt-14 px-6 container my-3 ",
-            ),
-            html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.Div(
+                    children=[html.Span(children="Smart Sensor Dashboard",
+                                        className="logo font-[20px] font-bold text-[#6439ff]")],
+                    className="top h-[50px] flex items-center justify-center"
+                ),
+                html.Hr(className="h-[0px] border-[0.5px] border-[lightgray]"),
+                html.Div(
+                    children=html.Ul(children=[
+                        html.Li(children=[
+                            html.Span(children="Sensors"),
+                        ]),
+                        html.Li(children=[
+                            html.Span(children="Sensors"),
+                        ]),
+                        html.Li(children=[
+                            html.Span(children="Sensors"),
+                        ]),
+                    ]),
+                    className="center pl-10"
+                ),
+                html.Div(
+                    children="color options",
+                    className="bottom"
+                )
+            ],
+            className="sidebar bg-white basis-1/6 border-r-[0.5px] border-[lightgray] min-h-[100vh]"
+        ),
+        html.Div(
+            children=[
                 html.Div(
                     children=[
                         html.Div(
                             children=[
-                                f"{sensor_count}",
-                                html.Br(),
-                                html.Span("Total # of Sensors", className="text-lg font-bold ml-4"),
+                                html.Div("About", className="pl-2 pr-1"),
+                                html.Div("Team", className="pl-2 pr-1"),
+                                html.Div("Contact Us", className="pl-2 pr-1"),
                             ],
-                            className=" shadow-xl py-3 px-12 text-5xl bg-[#1d3557] text-white text-center font-bold text-gray-800",
-                        ),
-                        html.Div(
-                            children=[
-                                active_sensors,
-                                html.Br(),
-                                html.Span("Active", className="text-lg font-bold ml-4"),
-                            ],
-                            className=" shadow-xl py-3 px-12 text-5xl bg-[#76c893] text-white text-center font-bold text-gray-800",
-                        ),
-                        html.Div(
-                            children=[
-                                inactive_sensors,
-                                html.Br(),
-                                html.Span("Inactive", className="inline-flex items-center text-lg font-bold"),
-                            ],
-                            className=" shadow-xl py-3 px-12 text-5xl bg-[red] text-white text-center font-bold text-gray-800",
-                        ),
-                        html.Div(
-                            children=[
-                                avr_speed,
-                                html.Br(),
-                                html.Span("Average Speed", className="inline-flex items-center text-lg font-bold"),
-                            ],
-                            className=" shadow-xl py-3 px-12 text-5xl bg-[#646ffa] text-white text-center font-bold text-gray-800",
-                        ),
-                        
+                            className="wrapper flex w-[100%] justify-end"
+                        )
                     ],
-                    className="my-4 w-full grid grid-flow-rows grid-cols-1 lg:grid-cols-4 gap-y-4 lg:gap-[60px]",
+                    className="navbar h-[50px] flex items-center font-[14px] text-[#555]"
                 ),
-                className="flex max-w-full justify-between items-center ",
-            ),
-            html.Div(
-                children=[
-                    html.Div(
-                        children=[
-                            html.Img(src="../assets/gym_photo.png", alt="gym photo"),
-                            html.Div()
-                        ],
-                        className="bg-white",
-                    ),
-                    html.Div(
-                        children=[
-                            dcc.Graph(id="example-graph2", figure=fig),
-                        ],
-                        className="w-full shadow-2xl rounded-sm",
-                    ),
-                ],
-                className="grid grid-cols-1 lg:grid-cols-2 gap-4",
-            ),
-        ],
-        className="bg-[#ebeaee]  flex py-14 flex-col items-center justify-center ",
-    ),
-    className="bg-[#ebeaee] container mx-auto px-14 py-4",
+                html.Hr(className="h-[0px] border-[0.5px] border-[lightgray]"),
+                html.Div(
+                    children=[
+                        generate_infocard("TOTAL SENSORS", sensor_count, "20%", ),
+                        generate_infocard("ACTIVE", active_sensors, "20%", ),
+                        generate_infocard("INACTIVE", inactive_sensors, "20%", ),
+                        generate_infocard("AVERAGE SPEED", avr_speed, "20%", ),
+                    ],
+                    className="flex p-[20px] gap-[20px]",
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children=[
+                                html.Img(src="../assets/gym_photo.png", alt="gym photo"),
+                                html.Div()
+                            ],
+                            className="bg-white",
+                        ),
+                        html.Div(
+                            children=[
+                                dcc.Graph(id="example-graph2", figure=fig),
+                            ],
+                            className="w-full shadow-2xl rounded-sm",
+                        ),
+                    ],
+                    className="grid grid-cols-1 lg:grid-cols-2 gap-4",
+                ),
+            ],
+            className="bg-white basis-5/6",
+        ),
+    ],
+    className="flex",
 )
 
 if __name__ == "__main__":
